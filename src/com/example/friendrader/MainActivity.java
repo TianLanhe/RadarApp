3258b56f67c673db254abe87068bd84478999693
HeZhenyu <849416763@qq.com>
2016-10-16 01:49:27 +0800

    second commit

--- src/com/example/friendrader/MainActivity.java
+++ src/com/example/friendrader/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.friendrader;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 
 import android.app.Activity;
@@ -10,33 +11,50 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
-
 public class MainActivity extends Activity {
-	
+
 	private Button friends;
 	private Button enemies;
 	private MapView mapview;
+	private BaiduMap baidumap;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext());
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        
-        mapview=(MapView) findViewById(R.id.mapView);
-        friends=(Button) findViewById(R.id.btn_friends);
-        enemies=(Button) findViewById(R.id.btn_enemies);
-        
-        WindowManager windowmanager=(WindowManager) getSystemService(Context.WINDOW_SERVICE);
-		int width=windowmanager.getDefaultDisplay().getWidth();
-        friends.setWidth(width/2);
-        enemies.setWidth(width/2);
-    }
-	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SDKInitializer.initialize(getApplicationContext());
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+
+		mapview = (MapView) findViewById(R.id.mapView);
+		baidumap = mapview.getMap();
+		friends = (Button) findViewById(R.id.btn_friends);
+		enemies = (Button) findViewById(R.id.btn_enemies);
+
+		// 设置两个按钮长度为屏幕的一半长
+		WindowManager windowmanager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		int width = windowmanager.getDefaultDisplay().getWidth();
+		friends.setWidth(width / 2);
+		enemies.setWidth(width / 2);
+
+		// 禁止地图所有手势操作
+		baidumap.getUiSettings().setAllGesturesEnabled(false);
+	}
+
 	@Override
-	protected void onDestroy(){
+	protected void onDestroy() {
 		super.onDestroy();
 		mapview.onDestroy();
+	}
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+		mapview.onPause();
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		mapview.onResume();
 	}
 }
--- src/com/example/friendrader/People.java
+++ src/com/example/friendrader/People.java
@@ -5,5 +5,63 @@ public class People {
 	private String phone_num;
 	private double longitude;
 	private double latitude;
-	private long lastupdatetime;
+	private long last_update_time;
+	private double distance;
+
+	People(String name, String num) {
+		this.name = name;
+		phone_num = num;
+		last_update_time = 0;
+		longitude = 0;
+		latitude = 0;
+		distance = 0;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	void setPhoneNum(String num) {
+		phone_num = num;
+	}
+
+	String getPhoneNum() {
+		return phone_num;
+	}
+
+	void setLongitude(double x) {
+		longitude = x;
+	}
+
+	double getLongitude() {
+		return longitude;
+	}
+
+	void setLatitude(double x) {
+		latitude = x;
+	}
+
+	double getLatitude() {
+		return latitude;
+	}
+
+	void setDistance(double x) {
+		distance = x;
+	}
+
+	double getDistance() {
+		return distance;
+	}
+
+	void setUpdateTime(long time) {
+		last_update_time = time;
+	}
+
+	long getLastUpdate() {
+		return last_update_time;
+	}
 }
